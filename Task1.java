af3c749ec0bea9e295c1af267f62f987950c5720
Vetrov Yury <888vetrov@gmail.com>
2022-09-11 09:49:42 +0300

    Tower of Hanoi

---
+++ Task1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Task1 {
+class GFG {
+    static void towerOfHanoi(int n, char from_rod,
+                             char to_rod, char aux_rod)
+    {
+        if (n == 0) {
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        System.out.println("Переместитe диск " + n + " из стержня "
+                           + from_rod + " на стержень "
+                           + to_rod);
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
+
+    public static void main(String args[])
+    {
+        int N = 3;
+ 
+        // А, В и С — названия стержней
+        towerOfHanoi(N, 'A', 'C', 'B');
+    }
+}
+}
